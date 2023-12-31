b4836dc975fdb6fe9655c6251959e167162ac759
anasalbarzawi <79091986+anasalbarzawi@users.noreply.github.com>
2023-12-31 18:07:47 +0300

    LapTime

--- Assets/Standard Assets/Vehicles/Car/Scripts/SaveScript.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/SaveScript.cs
@@ -8,15 +8,40 @@ public class SaveScript : MonoBehaviour
 
     public static float Speed;
     public static float TopSpeed;
+    public static int Gear;
+    public static int LapNumber;
+    public static bool LapChange = false;
+    public static float LapTimeMinutes;
+    public static float LapTimeSeconds;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (LapChange == true)
+        {
+            LapChange = false;
+            LapTimeMinutes = 0f;
+            LapTimeSeconds = 0f;
+           
+        }
+
+        if (LapNumber >= 1)
+        {
+            LapTimeSeconds = LapTimeSeconds + 1 * Time.deltaTime;
+            
+        }
+
+        if (LapTimeSeconds > 59)
+        {
+            LapTimeSeconds = 0f;
+            LapTimeMinutes++;
+        }
     }
 }
--- Assets/Standard Assets/Vehicles/Car/Scripts/UIScript.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/UIScript.cs
@@ -8,18 +8,57 @@ public class UIScript : MonoBehaviour
     public Image SpeedRing;
     private float DisplaySpeed;
     public Text SpeedText;
+    public Text GetText;
+    public Text LapNumberText;
+    public Text TotalLapsText;
+    public int TotalLaps = 3;
+    public Text LapTimeMinutesText;
+    public Text LapTimeSecondsText;
+
     // Start is called before the first frame update
     void Start()
     {
         SpeedRing.fillAmount = 0;
         SpeedText.text = "0";
+        GetText.text = "1";
+        LapNumberText.text = "0";
+        TotalLapsText.text = "/" + TotalLaps.ToString();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Speedometer
         DisplaySpeed = SaveScript.Speed / SaveScript.TopSpeed;
         SpeedRing.fillAmount = DisplaySpeed;
         SpeedText.text = (Mathf.Round(SaveScript.Speed).ToString());
+        GetText.text = (SaveScript.Gear + 1).ToString();
+
+        //LapNumber
+        LapNumberText.text = SaveScript.LapNumber.ToString();
+
+
+        //LapTime
+        if (SaveScript.LapTimeMinutes <= 9)
+        {
+            LapTimeMinutesText.text = "0" + (Mathf.Round(SaveScript.LapTimeMinutes).ToString()) + ":";
+        }
+        else if (SaveScript.LapTimeMinutes >= 10)
+        {
+            LapTimeMinutesText.text = (Mathf.Round(SaveScript.LapTimeMinutes).ToString()) + ":";
+        }
+
+        if (SaveScript.LapTimeSeconds <= 9)
+        {
+            LapTimeSecondsText.text = "0" + (Mathf.Round(SaveScript.LapTimeSeconds).ToString());
+        }
+
+        else if (SaveScript.LapTimeSeconds >= 10)
+        {
+            LapTimeSecondsText.text = (Mathf.Round(SaveScript.LapTimeSeconds).ToString());
+        }
+        
+
     }
 }
